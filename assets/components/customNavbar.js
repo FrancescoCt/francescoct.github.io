68adf6a56650d654c5aa9072594655c7a7ec3268
Francesco Catania <francesco.catania@dedagroup.it>
2025-05-22 14:23:53 +0200

    Introduzione componente menu orizzontale

--- assets/components/customNavbar.js
+++ assets/components/customNavbar.js
@@ -1,23 +1,38 @@
 class CustomHorizontalNavbar extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: "open" });
 
     // Recupera le coppie titolo-link dall'attributo "items"
+    const urlHome = this.getAttribute("home") || "#";
     const items = this.getAttribute("items")?.split(";") || [];
 
     // Creazione della struttura del menu
-    const menu = document.createElement("nav");
-    menu.innerHTML = `<ul style="list-style:none; padding:0;">${
-      items.map(item => {
-        const [title, link] = item.split(",");
-        return `<li><a href="${link.trim()}">${title.trim()}</a></li>`;
-      }).join("")
-    }</ul>`;
-
-    this.shadowRoot.appendChild(menu);
+    if (items) {
+      document.body.insertAdjacentHTML("beforeend", `
+        <nav>
+            <div class="logo"><a href="${urlHome}">Francesco Catania</a></div>
+            <input type="checkbox" id="menu-toggle">
+            <label for="menu-toggle" class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+            <ul class="menu">
+                ${
+                  items.map(item => {
+                    const [title, link] = item.split(",");
+                    return `<li class="nav-item"><a href="${link.trim()}">${title.trim()}</a></li>`;
+                  }).join("")
+                }
+            </ul>
+        </nav>
+      `);
+    }
+    // Dopo aver inserito gli elementi, rimuove il tag dal DOM
+    this.remove();
   }
 }
 
 // Registra il Custom Element
 customElements.define("custom-horizontal_navbar", CustomHorizontalNavbar);
+
